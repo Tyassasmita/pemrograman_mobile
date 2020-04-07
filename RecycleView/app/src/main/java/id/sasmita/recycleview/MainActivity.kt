371d6a2ca8b8f6ab6aed82159b33ae116ac68ec6
Tyassasmita <Tyassasmita12@gmail.com>
2020-04-07 16:59:39 +0700

    Recycle View

--- RecycleView/app/src/main/java/id/sasmita/recycleview/MainActivity.kt
+++ RecycleView/app/src/main/java/id/sasmita/recycleview/MainActivity.kt
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     val list = ArrayList<Users>()
-    val listUsers= arrayOf(
+    val listUsers = arrayOf(
         "Alice",
         "Boo",
         "Chahee",
@@ -19,18 +21,19 @@ class MainActivity : AppCompatActivity() {
         "Ivy",
         "Je"
     )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         RecyclerView.setHasFixedSize(true)
         RecyclerView.layoutManager = LinearLayoutManager(this)
-        for (i in 0 until listUsers.size){
+        for (i in 0 until listUsers.size) {
             list.add(Users(listUsers.get(i)))
-            if(listUsers.size - 1 == i){
-                val adapter = Adapter (list)
+            if (listUsers.size - 1 == i) {
+                val adapter = Adapter(list)
                 adapter.notifyDataSetChanged()
                 RecyclerView.adapter = adapter
             }
-        }
+         }
     }
 }
