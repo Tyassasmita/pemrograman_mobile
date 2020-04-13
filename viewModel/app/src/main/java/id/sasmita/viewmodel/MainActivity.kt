c8588974283b1f0cba143f131512b4908b1b2c64
Tyassasmita <Tyassasmita12@gmail.com>
2020-04-13 15:07:43 +0700

    ViewModel

---
+++ viewModel/app/src/main/java/id/sasmita/viewmodel/MainActivity.kt
@@ -0,0 +1,43 @@
+package id.sasmita.viewmodel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    lateinit var viewModel: MainViewModel
+    lateinit  var tvScoreA: TextView
+    lateinit  var tvScoreB: TextView
+    lateinit  var btnPlayerA: Button
+    lateinit  var btnPlayerB: Button
+    private lateinit var mainViewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initView()
+        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        tvScoreA.text = mainViewModel.initialCountA.toString()
+        tvScoreB.text = mainViewModel.initialCountB.toString()
+    }
+    private fun initView() {
+        tvScoreA = findViewById(R.id.tvScorePlayerA)
+        tvScoreB = findViewById(R.id.tvScorePlayerB)
+        btnPlayerA = findViewById(R.id.btnPlayerA)
+        btnPlayerB = findViewById(R.id.btnPlayerB)
+        btnPlayerA.setOnClickListener(this)
+        btnPlayerB.setOnClickListener(this)     }
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btnPlayerA ->
+                tvScoreA.text = mainViewModel.currentCountA.toString()
+            R.id.btnPlayerB ->
+                tvScoreB.text = mainViewModel.currentCountB.toString()
+        }
+    }
+}
+
---
+++ viewModel/app/src/main/java/id/sasmita/viewmodel/MainViewModel.kt
@@ -0,0 +1,20 @@
+package id.sasmita.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    var initialCountA: Int = 0
+        private set
+    var initialCountB = 0
+        private set
+    val currentCountA: Int
+        get() {
+            initialCountA++
+            return initialCountA
+        }
+    val currentCountB: Int
+        get() {
+            initialCountB++
+            return initialCountB
+        }
+}
